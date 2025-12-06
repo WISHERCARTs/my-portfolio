c53b8fcf4ec2363d9835cf850779a5cddfaca901
Wish Nakthong <wishza2549@gmail.com>
2025-12-07 01:01:43 +0700

    chore: remove file

--- components/test_types.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useChat } from "@ai-sdk/react";
-
-export function Test() {
-  const { input, handleInputChange, messages, isLoading, handleSubmit } =
-    useChat();
-  console.log(input, handleInputChange, messages, isLoading, handleSubmit);
-  return <div>Test</div>;
-}
